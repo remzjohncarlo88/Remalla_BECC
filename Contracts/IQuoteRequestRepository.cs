d35ee83b466b28a06ab0ad729e0836168918b87f
remzjohncarlo88 <41571167+remzjohncarlo88@users.noreply.github.com>
2023-01-06 10:30:34 +0800

    Contracts

---
+++ Contracts/IQuoteRequestRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Contracts
+{
+    public interface IQuoteRequestRepository
+    {
+        Task<QuoteRequest> Get();
+
+        IEnumerable<Listing> GetQuotesByNumPassengers(int numPassengers);
+    }
+}
