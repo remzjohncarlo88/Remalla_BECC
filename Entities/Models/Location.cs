a63dd6dc7bda0662d126190b877ec353dbb44824
remzjohncarlo88 <41571167+remzjohncarlo88@users.noreply.github.com>
2023-01-06 10:31:58 +0800

    Entities

---
+++ Entities/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Location
+    {
+        public string? IP { get; set; }
+        public string? ContinentCode { get; set; }
+        public string? ContinentName { get; set; }
+        public string? CountryCode { get; set; }
+        public string? CountryName { get; set; }
+        public string? RegionName { get; set; }
+        public string? City { get; set; }
+    }
+}
